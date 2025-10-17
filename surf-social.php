4c9078482ecd547475bc9e84e21c0f289e1cb53c
Thomas Fraher <tommyfraher@gmail.com>
2025-10-16 18:59:41 -0700

    a

--- surf-social.php
+++ surf-social.php
@@ -4,7 +4,7 @@ Plugin Name: Surf Social
 Plugin URI: https://github.com/tommypf11/surf-social
 GitHub Plugin URI: https://github.com/tommypf11/surf-social
 Description: Your plugin description
-Version: 1.0.9
+Version: 1.0.10
 Author: Thomas Fraher
 */
 
